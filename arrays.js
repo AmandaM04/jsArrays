3546af8d798989bda623360bd066d035c2c35fad
Amanda Mitchell <wreedamitchell@gmail.com>
2018-02-27 21:18:30 -0600

    challenge book complete

--- arrays.js
+++ arrays.js
@@ -14,3 +14,23 @@ if(test === opp){
 }else{
     console.log("same? ", false); 
 }
+
+// var animals = ['fish', 'dog', 'cat']
+// var animalsElement = document.getElementById('animalz');
+// for(var i=0; i<animals.length; i++){
+//     // console.log("animals:", animals[i])
+//     animalzElement.innerHTML += '<h5>' + animals[i] + '</h5>';
+// }
+
+var book1 = "catch 22";
+// var book2 = "Slaughterhouse 5";
+// var book3 = "Fahrenheit 451";
+var bookSplit = book.split("");
+var realNumz = [];
+for(var j=0; j<booksplit.length; j++){
+    if((bookSplit[j]*1)){
+        realNumz.push(bookSplit[j]);
+    }
+}
+        console.log("answer", realNumz.join(""))
+
