43ad529993cac84d3f901edcde8b4dbad0e7740c
Amanda Mitchell <wreedamitchell@gmail.com>
2018-02-27 19:46:41 -0600

    challenge 1 complete

--- arrays.js
+++ arrays.js
@@ -1,1 +1,16 @@
-console.log();
+
+// Challenge #1
+
+var test1 = 'cat'; //false
+var test2 = 'racecar'; //true
+var test3 = 'bear'; //false
+var test4 = 'mom'; //true
+
+var test= "cat";
+var opp = test.split("").reverse().join("");
+
+if(test === opp){
+    console.log("same? ", true);
+}else{
+    console.log("same? ", false); 
+}
